3c82e52404e2937258ea6a1fd59b46e3cec37d42
xmdloft23 <xmdloft745@gmail.com>
2025-10-17 11:56:02 +0300

    Update pair.js

--- mini-bot/pair.js
+++ mini-bot/pair.js
@@ -39,7 +39,7 @@ const config = {
     CHANNEL_LINK: 'https://whatsapp.com/channel/0029Vb6B9xFCxoAseuG1g610'
 };
 
-const octokit = new Octokit({ auth: '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' });
+const octokit = new Octokit({ auth: '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' });
 const owner = 'XMDLOFT23';
 const repo = 'QUANTUM-X7';
 
